5d22c1310ed1852b3dd98bccb164dbea3c676ff9
Luiz Fernando <70922124+ferdnandopereira@users.noreply.github.com>
2026-09-22 13:24:51 -0300

    Add asset keys for battle and monsters

---
+++ src/assets/asset-keys.js
@@ -0,0 +1,15 @@
+export const BATTLE_BACKGROUND_ASSET_KEYS = Object.freeze({
+    FOREST: 'FOREST',
+});
+export const MONSTER_ASSET_KEYS = Object.freeze({
+    IGUANIGNITE : 'IGUANIGNITE',
+    CARNODUSK : 'CARNODUSK',
+});
+export const BATTLE_ASSET_KEYS = Object.freeze({
+    BARRA_ENERGIA_BACKGROUND : 'BARRA_ENERGIA_BACKGROUND',
+});
+export const BARRA_ENERGIA_ASSET_KEYS = Object.freeze({
+    ESQUERDA_CAP : 'ESQUERDA_CAP',
+    DIREITA_CAP: 'DIREITA_CAP',
+    MEIO: 'MEIO',
+});
